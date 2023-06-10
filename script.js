58af034085ef57215d0d1ecac0b9f6c2764bc448
unknown <izurumw@gmail.com>
2023-06-10 17:39:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(){
+    var fNum=document.getElementById("num1").value;
+    var sNum =document.getElementById("num2").value;
+   
+    var sum=+fNum+ +sNum;
+    console.log(sum);
+}
+function min(){
+    var fNum=document.getElementById("num1").value;
+    var sNum =document.getElementById("num2").value;
+   
+    var sum=(fNum-sNum);
+    console.log(sum);
+}
+function mul(){
+    var fNum=document.getElementById("num1").value;
+    var sNum =document.getElementById("num2").value;
+   
+    var sum=(fNum*sNum);
+    console.log(sum);
+}
+function dev(){
+    var fNum=document.getElementById("num1").value;
+    var sNum =document.getElementById("num2").value;
+   
+    var sum=(fNum/sNum);
+    console.log(sum);
+}
+
+
+
